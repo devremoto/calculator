7f01669e35b41d3cf4657e7bb60b73ca9a370489
devremoto <34072596+devremoto@users.noreply.github.com>
2023-10-01 13:00:44 +0200

    Delete calculator directory

--- calculator/StringExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿static class StringExtensions
-{
-    public static string ReplaceAt(this string str, int index, char newChar)
-    {
-        return str.Remove(index, 1).Insert(index,newChar.ToString()); 
-    }
-}
