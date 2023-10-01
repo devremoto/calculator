fe9ca5ce17ce97134f2506963fd4cd056d651967
devremoto <adilson@almeidapedro.com.br>
2023-10-01 14:07:44 +0200

    refactory

--- coverage.bat
+++ coverage.bat
@@ -1,11 +1,10 @@
 taskkill /IM chrome.exe /F
 
-set report_dir=.\Caculator.Test\reports
-set filters="-assemblyFilters:+*.*" "-classfilters:-*Test;-*Program;-*StringExtensions"
-set filters=""
+set report_dir=.\Calculator.Test\reports
+del %report_dir% /Q
 
-dotnet-coverage collect -f xml -o %report_dir%\coverage.xml dotnet test 
-reportgenerator -reports:%report_dir%\coverage.xml -targetdir:%report_dir% %filters%
+dotnet test --no-build  /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov,cobertura\" /p:Exclude=\"[*]Program*,[*]StringExtensions*\" /p:CoverletOutput=reports/ Calculator.Test 
 
+reportgenerator -reports:%report_dir%\coverage.cobertura.xml -targetdir:%report_dir% %filters%
 
 start %report_dir%\index.html
